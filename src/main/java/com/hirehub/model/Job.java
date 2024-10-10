0f0cafc783d861593fd69c82689cc13a013798e0
Roxy Khalilifar <roxykhal@hotmail.com>
2024-10-10 15:00:40 +0100

    first commit

---
+++ src/main/java/com/hirehub/model/Job.java
@@ -0,0 +1,85 @@
+package com.hirehub.model;
+
+import java.util.Date;
+
+public class Job {
+    private int id;
+    private String title;
+    private String description;
+    private String requirements;
+    private Date postingDate;
+    private Date closingDate;
+    private String status;
+
+    // Constructors
+    public Job() {
+    }
+
+    public Job(String title, String description, String requirements, Date postingDate, Date closingDate,
+            String status) {
+        this.title = title;
+        this.description = description;
+        this.requirements = requirements;
+        this.postingDate = postingDate;
+        this.closingDate = closingDate;
+        this.status = status;
+    }
+
+    // Getters and Setters
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getRequirements() {
+        return requirements;
+    }
+
+    public void setRequirements(String requirements) {
+        this.requirements = requirements;
+    }
+
+    public Date getPostingDate() {
+        return postingDate;
+    }
+
+    public void setPostingDate(Date postingDate) {
+        this.postingDate = postingDate;
+    }
+
+    public Date getClosingDate() {
+        return closingDate;
+    }
+
+    public void setClosingDate(Date closingDate) {
+        this.closingDate = closingDate;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+}
