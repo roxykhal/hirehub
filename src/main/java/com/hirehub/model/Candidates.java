32ab048306b3a54691aee5cd9477466baac69755
Roxy Khalilifar <roxykhal@hotmail.com>
2024-10-11 11:30:16 +0100

    candidate class

--- src/main/java/com/hirehub/model/Candidates.java
+++ src/main/java/com/hirehub/model/Candidates.java
@@ -6,5 +6,71 @@ public class Candidates {
     private String resumeURL;
     private Date registrationDate;
 
+// Constructors
+    public Candidates() {
+    }
+
+    public Candidates(String firstName, String lastName, String emailAddress, int phoneNumber, String resumeURL, Date registrationDate) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.emailAddress = emailAddress;
+        this.phoneNumber = phoneNumber;
+        this.resumeURL = resumeURL;
+        this.registrationDate = registrationDate;
+    }
+
+    //getters and setters
+
+    public String getfirstName() {
+        return firstName;
+    }
+
+    public void setfirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getlastName() {
+        return lastName;
+    }
+
+    public void setlastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getemailAddress() {
+        return emailAddress;
+    }
+
+    public void setemailAddress(String emailAddress) {
+        this.emailAddress = emailAddress;
+    }
+
+    public int getphoneNumber() {
+        return phoneNumber;
+    }
+
+    public void setphoneNumber(int phoneNumber) {
+        this.phoneNumber = phoneNumber;
+    } 
+
+    public String getresumeURL() {
+        return resumeURL;
+
+    }
+
+    public void setresumeURL(String resumeURL) {
+        this.resumeURL = resumeURL;
+    }
+
+    public date getregistrationDate() {
+        return registrationDate;
+    
+    }
+
+    public void setregistrationDate(Date registrationDate) {
+        this.registrationDate = registrationDate;
+    }
+
 
 }
+
