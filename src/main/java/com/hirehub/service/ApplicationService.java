15bc4e9ac6bd36b9a86e515dca00700140e0dd7e
Roxy Khalilifar <roxykhal@hotmail.com>
2024-12-30 16:31:23 +0000

    add usersService

---
+++ src/main/java/com/hirehub/service/ApplicationService.java
@@ -0,0 +1,5 @@
+package com.hirehub.service;
+
+public class ApplicationService {
+    
+}
---
+++ src/main/java/com/hirehub/service/UserService.java
@@ -0,0 +1,43 @@
+package com.hirehub.service;
+import com.hirehub.dao.UsersDAO;
+import com.hirehub.dao.UsersDAOIMPL;
+import com.hirehub.model.Users;
+import java.util.List;
+
+public class UserService {
+    private UsersDAO usersDAO;
+
+    public UserService() {
+        this.usersDAO = new UsersDAOIMPL();
+
+    }
+
+    public void CreateUsers(Users users) {
+        usersDAO.add(users);
+    }
+
+    public void UpdateUsers(Users users) {
+        usersDAO.update(users);
+    }
+
+    public void DeleteUsers(Users users) {
+        usersDAO.delete(0);
+    }
+
+    public void getUsersID(Users users) {
+        usersDAO.getuserId(0);
+    }
+
+
+
+
+
+
+
+
+
+
+
+
+    
+}
