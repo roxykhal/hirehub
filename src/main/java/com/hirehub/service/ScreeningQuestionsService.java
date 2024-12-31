2106e879de180ca4b9a3a3c28af726e2e2834420
Roxy Khalilifar <roxykhal@hotmail.com>
2024-12-31 10:08:12 +0000

    add screeningQuestionsService

---
+++ src/main/java/com/hirehub/service/ScreeningQuestionsService.java
@@ -0,0 +1,35 @@
+package com.hirehub.service;
+import com.hirehub.dao.ScreeningQuestionsDAO;
+import com.hirehub.dao.ScreeningQuestionsDAOImpl;
+import com.hirehub.model.ScreeningQuestions;
+import java.util.List;
+
+public class ScreeningQuestionsService {
+    private ScreeningQuestionsDAO screeningQuestionsDAO;
+
+    public ScreeningQuestionsService() {
+        this.screeningQuestionsDAO = new ScreeningQuestionsDAOImpl();
+    }
+
+public void createScreeningQuestions() {
+    screeningQuestionsDAO.add(null);
+}
+
+public void updateScreeningQuestions() {
+    screeningQuestionsDAO.update(null);
+}
+
+public void deleteScreeningQuestions(int id) {
+    screeningQuestionsDAO.delete(null);
+    
+}
+
+public void getScreeningQuestionsById(int id) {
+    screeningQuestionsDAO.getByID(id)
+}
+
+public List<ScreeningQuestions> getAllScreeningQuestions() {
+    return screeningQuestionsDAO.getAll()
+}
+
+}
