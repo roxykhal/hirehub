5f66b69f9c80dc6e0333e4f6ba0da78555b9feec
Roxy Khalilifar <roxykhal@hotmail.com>
2024-12-31 10:02:04 +0000

    add OffersService

---
+++ src/main/java/com/hirehub/service/OffersService.java
@@ -0,0 +1,36 @@
+package com.hirehub.service;
+import com.hirehub.dao.OffersDAO;
+import com.hirehub.dao.OffersDAOImpl;
+import com.hirehub.model.Offers;
+import java.util.List;
+
+
+public class OffersService {
+    private OffersDAO offersDAO;
+
+    public OffersService() {
+        this.offersDAO = new OffersDAOImpl();
+    }
+
+public void createOffers() {
+    offersDAO.add(null);
+}
+
+public void updateOffers() {
+    offersDAO.update(null);
+}
+
+public void deleteOffers(int id) {
+    offersDAO.delete(null);
+}
+
+public void getOfferById(int id) {
+    offersDAO.getByID(id);
+}
+
+public List<Offers> getAllOffers() {
+    return offersDAO.getAll();
+}
+
+    
+}
