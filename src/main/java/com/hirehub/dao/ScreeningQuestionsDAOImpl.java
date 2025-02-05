64770401d86ad98f1e74bc707431a4f4813b2a94
Roxy Khalilifar <roxykhal@hotmail.com>
2025-02-05 22:19:56 +0000

    getAll screeningQuestions method DAO Impl

--- src/main/java/com/hirehub/dao/ScreeningQuestionsDAOImpl.java
+++ src/main/java/com/hirehub/dao/ScreeningQuestionsDAOImpl.java
@@ -3,6 +3,8 @@ package com.hirehub.dao;
 import com.hirehub.model.ScreeningQuestions;
 import com.hirehub.util.DatabaseConnection;
 import java.sql.*;
+import java.util.ArrayList;
+import java.util.List;
 
 
 public class ScreeningQuestionsDAOImpl implements ScreeningQuestionsDAO{
@@ -77,11 +79,39 @@ public class ScreeningQuestionsDAOImpl implements ScreeningQuestionsDAO{
         }
         return null;
         }
+    
+
+    @Override
+public List<ScreeningQuestions> getAll() {
+    List<ScreeningQuestions> screeningsList = new ArrayList<>();
+    String sql = "SELECT * FROM screening_questions";
+
+    try (Statement stmt = connection.createStatement();
+         ResultSet rs = stmt.executeQuery(sql)) {
+
+        while (rs.next()) {
+            screeningsList.add(extractScreeningQuestionsFromResultSet(rs));  // Use screeningsList here
+        }
+
+    } catch (SQLException e) {
+        e.printStackTrace();
     }
 
+    return screeningsList;  // Return the correct list
+}
+
+
+private ScreeningQuestions extractScreeningQuestionsFromResultSet(ResultSet rs) throws SQLException {
+    ScreeningQuestions screeningQuestions = new ScreeningQuestions();
+
+    screeningQuestions.setjobID(rs.getInt("job_id"));
+    screeningQuestions.settext(rs.getString("screening_questions"));
+
+    return screeningQuestions;
     
 
-    
+}
+}
      
 
         
