8bc36522d8523b7f414ae1531b93375a17282699
Roxy Khalilifar <roxykhal@hotmail.com>
2025-03-03 23:18:53 +0000

    fix salary add method in offersDAOimpl

--- src/main/java/com/hirehub/dao/OffersDAOImpl.java
+++ src/main/java/com/hirehub/dao/OffersDAOImpl.java
@@ -24,7 +24,7 @@ public class OffersDAOImpl implements OffersDAO {
             pstmt.setString(2, offers.getstatus());
             pstmt.setInt(3, offers.getapplicationID());
             pstmt.setDate(4, new java.sql.Date(offers.getstartDate().getTime()));
-            pstmt.setBoolean(5, offers.getsalary());
+            pstmt.setBigDecimal(5, offers.getsalary());
 
     pstmt.executeUpdate();
 
